34e1583dbeda91b51ab1f57f286cf9d2a49a60ec
sugim0t0 <sugimoto.shun@gmail.com>
2018-03-31 11:51:39 +0900

    Modified to be created MAC header in module

--- modules/hook_mod/hook_mod.c
+++ modules/hook_mod/hook_mod.c
@@ -1,7 +1,7 @@
 /**
  * Send/Recv packets hook module by netfilter
  * Test Linux kernel version: 4.9.80
- * 
+ *
  * @author Sugimoto
  */
 #include <linux/module.h>
@@ -12,6 +12,8 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/skbuff.h>
 #include <linux/tcp.h>
+#include <net/arp.h>
+#include <net/route.h>
 
 #define RECV_MODE 0
 #define SEND_MODE 1
@@ -19,15 +21,17 @@
 #define HOOK_MODE SEND_MODE
 
 #if (HOOK_MODE == RECV_MODE)
-/* 172.29.46.215 */
-#define DST_IP_ADDRESS 0xAC1D2ED7
+/* 172.29.46.79 */
+#define DST_IP_ADDRESS 0xAC1D2E4F
 #else
-/* 172.29.47.105 */
-#define DST_IP_ADDRESS 0xAC1D2F69
+/* 172.29.47.152 */
+#define DST_IP_ADDRESS 0xAC1D2F98
 #endif
 #define PORT_NUMBER 12345
 
-#define DEBUG_PRINT 1
+#define RASPI_CKSUM_OFFLOAD 1
+#define HOOK_PACKET_DROP    0
+#define DEBUG_PRINT         1
 
 MODULE_DESCRIPTION("Send/Recv packets hook module by netfilter");
 MODULE_AUTHOR("sugimoto");
@@ -46,9 +50,22 @@ void dump_data(unsigned char* data, int size)
     }
 }
 
-uint16_t calc_cksum(unsigned char* data, int size)
+void print_skb_info(struct sk_buff *skb)
 {
-    uint32_t sum = 0;
+    printk("---- SKBUF info ----\n");
+    printk("len:       %u\n", skb->len);
+    printk("data_len:  %u\n", skb->data_len);
+    printk("head addr: %p\n", skb->head);
+    printk("data addr: %p\n", skb->data);
+    printk("tail addr: %p\n", skb->tail);
+    printk("end addr:  %p\n", skb->end);
+    printk("csum:      %u\n", skb->csum);
+    printk("--------------------\n");
+}
+
+uint16_t calc_cksum(unsigned char* data, uint16_t orig_sum, int size)
+{
+    uint32_t sum = orig_sum;
     uint16_t *ptr = (uint16_t*)data;
     int rest;
 
@@ -91,10 +108,29 @@ uint16_t calc_pseudo_ipv4_header(unsigned char* ip_hdr)
     pseudo_hdr.zero = 0;
     pseudo_hdr.ptcl = 6; // 6: TCP
     pseudo_hdr.length = htons(tcp_length);
-    sum = calc_cksum((unsigned char*)&pseudo_hdr, sizeof(pseudo_hdr));
+    sum = calc_cksum((unsigned char*)&pseudo_hdr, 0, sizeof(pseudo_hdr));
     dump_data((unsigned char*)&pseudo_hdr, sizeof(pseudo_hdr));
 
     return sum;
+}
+
+struct neighbour * get_neighbour(struct sk_buff *skb)
+{
+    struct neighbour *neigh;
+    struct dst_entry *dst = skb_dst(skb);
+    struct rtable *rt = (struct rtable *)dst;
+    struct net_device *dev = dst->dev;
+
+    uint32_t nexthop;
+    if(rt->rt_gateway)
+        nexthop = (uint32_t)rt->rt_gateway;
+    else
+        nexthop = (uint32_t)ip_hdr(skb)->daddr;
+    skb->dev = dev;
+
+    neigh = __ipv4_neigh_lookup_noref(dev, nexthop);
+
+    return neigh;
 }
 
 unsigned int main_hook
@@ -134,7 +170,11 @@ unsigned int main_hook
     /* Confirm destination port number in TCP header */
     if(be16_to_cpu(tcp_header->source) == PORT_NUMBER)
     {
-        int num_skb;
+#if HOOK_PACKET_DROP
+        return NF_DROP;
+#else
+        struct neighbour *neigh;
+        int num_skb = 1;
         uint16_t pseudo_sum;
         /* Scatter/Gather func. check */
 #if 0
@@ -144,10 +184,10 @@ unsigned int main_hook
             printk("Scatter/Gather -> ON\n");
         }
 #endif
-
         /* GSO func. check */
         struct tcp_sock *tp = tcp_sk(skb->sk);
 #if DEBUG_PRINT
+        printk("--------------------------\n");
         printk("socket sk_route_caps: %llx\n", skb->sk->sk_route_caps);
         printk("socket gso type: %d\n", skb->sk->sk_gso_type);
         printk("socket gso max size: %d\n", skb->sk->sk_gso_max_size);
@@ -167,8 +207,9 @@ unsigned int main_hook
         printk("tcp mss cache: %u\n", tp->mss_cache);
         printk("tcp gso_segs: %u\n", tp->gso_segs);
         printk("NETIF_F_GSO_SHIFT: %d\n", NETIF_F_GSO_SHIFT);
-        dump_data(skb->data, 40);
+        //dump_data(skb->data, 40);
 #endif
+#if 1
         if(!(skb->sk->sk_route_caps & (1 << NETIF_F_GSO_SHIFT)))
         {
             //netif_set_gso_max_size(in, 16*1460);
@@ -178,38 +219,72 @@ unsigned int main_hook
             tp->gso_segs = 16;
             skb->sk->sk_route_caps |= (1 << NETIF_F_GSO_SHIFT);
             printk("GSO -> ON\n");
-        }
-        printk("data_len: %u\n", skb->data_len);
-        printk("len: %u\n", skb->len);
-        num_skb = (skb->len - 40) / 1460;
-        if((skb->len - 40) % 1460)
-            num_skb++;
-        printk("num_skb: %d\n", num_skb);
-        printk("skb->ip_summed: %d\n", skb->ip_summed);
-#if 1
-        if(skb->len > 1500)
-        {
-            printk("IP len:%u\n", ntohs(ip_header->tot_len));
-            ip_header->tot_len = htons(1500);
-            skb->len = 1500;
-            skb->tail = skb->data + 1500;
-            // recalc IP checksum
-            printk("existed IP cksum: %02x\n", ip_header->check);
-            ip_header->check = 0;
-            ip_header->check = calc_cksum((unsigned char*)ip_header, 20);
-            printk("Recalc IP cksum: %02x\n", ip_header->check);
-            // recalc TCP checksum
-            // Existed TCP cksum value is calculated only part of pseudo IPv4 header.
-            printk("existed TCP cksum: %02x\n", tcp_header->check);
-            tcp_header->check = 0;
-            tcp_header->check = calc_pseudo_ipv4_header((unsigned char*)ip_header);
-            printk("Recalc TCP cksum: %02x\n", tcp_header->check);
-            // payload checksum
-            printk("TCP payload cksum: %02x\n", calc_cksum(skb->data, skb->len));
-            // sk_buff csum
-            printk("skb->csum: %02x\n", skb->csum);
+            printk("data_len: %u\n", skb->data_len);
+            printk("len: %u\n", skb->len);
+            num_skb = (skb->len - 40) / 1460;
+            if((skb->len - 40) % 1460)
+                num_skb++;
+            printk("num_skb: %d\n", num_skb);
+            printk("skb->ip_summed: %d\n", skb->ip_summed);
         }
 #endif
+
+        neigh = get_neighbour(skb);
+        if(neigh) {
+            int hh_len, hh_alen;
+            skb->protocol = htons(ETH_P_IP);
+#if !RASPI_CKSUM_OFFLOAD
+            // Calc IP checksum
+            printk("existed IP cksum: %02x\n", ntohs(ip_header->check));
+            ip_header->check = 0;
+            ip_header->check = calc_cksum((unsigned char*)ip_header, 0, 20);
+            printk("Recalc IP cksum: %02x\n", ntohs(ip_header->check));
+            // Calc TCP checksum
+            // Existed TCP cksum value is calculated only part of pseudo IPv4 header.
+            printk("existed TCP cksum: %02x\n", ntohs(tcp_header->check));
+            tcp_header->check = 0;
+            pseudo_sum = calc_pseudo_ipv4_header((unsigned char*)ip_header);
+            printk("Recalc TCP cksum: %02x\n", (uint16_t)~(ntohs(pseudo_sum)));
+            tcp_header->check = calc_cksum(skb->data + 20, ~(pseudo_sum), skb->len - 20); 
+            printk("TCP cksum: %02x\n", ntohs(tcp_header->check));
+            //dump_data(skb->data + 20, skb->len - 20);
+#endif /* !RASPI_CKSUM_OFFLOAD */
+            print_skb_info(skb);
+            // Segment Jumbo frame
+            if(skb->len > 1500) {
+                skb->len = 1500;
+                skb->tail = skb->data + 1500;
+                ip_header->tot_len = htons(1500);
+                // Recalc IP checksum
+                ip_header->check = 0;
+                ip_header->check = calc_cksum((unsigned char*)ip_header, 0, 20);
+                // Recalc TCP checksum
+                tcp_header->check = 0;
+#if !RASPI_CKSUM_OFFLOAD
+                pseudo_sum = calc_pseudo_ipv4_header((unsigned char*)ip_header);
+                tcp_header->check = calc_cksum(skb->data + 20, ~(pseudo_sum), skb->len - 20); 
+#else
+                tcp_header->check = calc_pseudo_ipv4_header((unsigned char*)ip_header);
+                printk("recalc tcp cksum: %02x\n", tcp_header->check);
+#endif
+            }
+            // Create MAC header
+            hh_len = neigh->hh.hh_len;
+            hh_alen = HH_DATA_ALIGN(hh_len);
+            printk("hh_len: %d\n", hh_len);
+            printk("hh_alen: %d\n", hh_alen);
+            printk("hh->data >\n");
+            dump_data(neigh->hh.hh_data, hh_alen);
+            memcpy(skb->data - hh_alen, neigh->hh.hh_data, hh_alen);
+            skb_push(skb, hh_len);
+            // Transmit sk_buff
+            dev_queue_xmit(skb);
+            return NF_STOLEN;
+        }
+        else {
+            printk("Neighbour is not existed yet.\n");
+        }
+#endif /* HOOK_PACKET_DROP */
     }
 
     return NF_ACCEPT; /* Pass */
